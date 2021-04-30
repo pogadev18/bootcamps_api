004884e77f780b55b408038c724b720818534679
Bogdan P <bogdan.pogacean@softvision.com>
2021-04-30 18:47:16 +0300

    advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,15 @@ const asyncHandler = require('../middleware/async');
  - @access  Public
  */
 exports.getBootcamps = asyncHandler(async (req, res) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+  let queryString = JSON.stringify(req.query);
+
+  queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${ match }`);
+
+  query = Bootcamp.find(JSON.parse(queryString));
+
+  const bootcamps = await query;
+
   res.status(200).json({
     success: true,
     count: bootcamps.length,
